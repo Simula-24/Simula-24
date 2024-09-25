55e0a4cb9af12a4464388b341b1c9a26e6899bb3
raging-loon <85407521+raging-loon@users.noreply.github.com>
2024-09-25 16:09:38 -0400

    added smart pointer

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,11 +2,10 @@
 #include <curses.h>
 #include <simula24/core/stl/copy_on_write.h>
 #include <simula24/core/stl/utility.h>
+#include <simula24/core/stl/smart_ptr.h>
 #include <utility>
 int main(int argc, char** argv)
 {
-    int x = 500;
-    int&& y = std::move(x);
-
-    printf("%d | %d\n", x, y);
+    simula24::shared_ptr<int> x = simula24::make_shared<int>(1);
+    printf("%d\n",*x);
 }
