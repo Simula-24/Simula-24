eeeb70c335c9095d107f9a3f449cde981a7f090c
raging-loon <85407521+raging-loon@users.noreply.github.com>
2024-09-25 12:49:19 -0400

    added unit-test scripts

---
+++ tests/test_main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    printf("Running unit tests...\n");
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
