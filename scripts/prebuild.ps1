1de82c4743aac77c51307c79e1cc34dc60aab446
raging-loon <85407521+raging-loon@users.noreply.github.com>
2024-10-05 15:32:04 -0400

    removed pdcurses, add PDB gen to release via cmake

--- scripts/prebuild.ps1
+++ scripts/prebuild.ps1
@@ -4,20 +4,7 @@ Import-Module $PSScriptRoot/buildutils.psm1 -DisableNameChecking
 $curdir = Get-Location
 $buildloc = "$curdir/build"
 mkdir -Force "$buildloc/" | out-null
-cd thirdparty/PDCurses
-
-if($clean -eq $true) {
-    Log-Info "Cleaning..."
-    rm -Force pdcurses.dir,x64,Debug,CMakeFiles
-}
-
-Log-Info "Configuring PDCurses..."
-cmake .
-Log-Info "Building PDCurses..."
-cmake --build .
-Log-Info "Done. Copying Files"
-cp Debug/pdcurses.* $buildloc
-cd $curdir
+Log-Info "Copying Files..."
 cp thirdparty/SDL2/lib/x64/*.lib $buildloc
 cp thirdparty/SDL2/lib/x64/*.dll $buildloc
 cp thirdparty/SDL2_ttf/lib/x64/*.lib $buildloc
